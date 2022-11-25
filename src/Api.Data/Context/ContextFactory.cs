404585cf918a7d94a1d74a29fbc347b5c17b0f66
getrojob <getrojob@gmai.com>
2022-11-24 21:28:58 -0300

    add projeto

---
+++ src/Api.Data/Context/ContextFactory.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Api.Data.Context
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        public MyContext CreateDbContext(string[] args)
+        {
+            // Usado para criar as Migrações em tempo de designer da aplicação
+
+            //var connectionString = "Server=.\\SQLEXPRESS;Initial Catalog=dbAPI_2;Trusted_Connection=True;MultipleActiveResultSets=true";
+            var connectionString = "Server=localhost;Port=3306;Database=dbAPI2;Uid=root;Pwd=vetrigo";
+
+            var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+            optionsBuilder.UseMySql(connectionString);
+            //optionsBuilder.UseSqlServer(connectionString);
+            return new MyContext(optionsBuilder.Options);
+        }
+    }
+}
